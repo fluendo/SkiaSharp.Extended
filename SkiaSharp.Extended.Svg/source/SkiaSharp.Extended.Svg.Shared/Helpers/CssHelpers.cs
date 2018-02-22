e637b354961c865fde20d647985d50495b724a40
Matthew Leibowitz <mattleibow@live.com>
2018-02-22 09:17:18 +0200

    Get it all to compile

--- SkiaSharp.Extended.Svg/source/SkiaSharp.Extended.Svg.Shared/Helpers/CssHelpers.cs
+++ SkiaSharp.Extended.Svg/source/SkiaSharp.Extended.Svg.Shared/Helpers/CssHelpers.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
 
-namespace SkiaSharp.Helpers
+namespace SkiaSharp
 {
 	internal static class CssHelper
 	{
